7f19e25ea503941a89d3bd3c4f320242d87e33f4
Tony Morris <afmorris@outlook.com>
2015-08-22 20:21:12 -0400

    Whoops, didn't mean to remove MVC services

--- LadyColtSoccer/src/LadyColtSoccer/Startup.cs
+++ LadyColtSoccer/src/LadyColtSoccer/Startup.cs
@@ -54,7 +54,7 @@ namespace LadyColtSoccer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddMvc();
         }
 
         // Configure is called after ConfigureServices is called.
